2cd4ed1e5eb86c646a65237f6f89088a6d8d5320
Kroiririoroirkk <taoeric9@gmail.com>
2020-05-09 20:57:09 -0400

    Updated Bokedex

--- bokedex.js
+++ bokedex.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
   $.getJSON('bokedex.json', function(data) {
     var items = [];
-    $.each(data, function(bokemon, desc) {
+    Object.keys(data).forEach(function(bokemon, i) {
       items.push($('<div/>', {
         'class': 'bokemon',
-        text: bokemon,
+        text: `#${i+1}) ${bokemon}`,
         click: function() {
-          $('#info').text(desc);
+          $('#info').text(data[bokemon]);
         }
       }));
     });
