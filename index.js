cc7cd3b24f1dd0465e8ad2dcc8975bcd8836c3ce
Kroiririoroirkk <taoeric9@gmail.com>
2020-05-13 13:43:40 -0400

    Fix typo

--- index.js
+++ index.js
@@ -212,7 +212,7 @@ function handleWSMessage(e) {
             break;
           case "s":
             game.addGameObj(new Sign(pos));
-            break;s
+            break;
         }
       }
     }
