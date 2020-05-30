ee227f30b82bfb2368bb9d054ac1551a27b819cb
Kroiririoroirkk <taoeric9@gmail.com>
2020-05-30 15:28:22 -0400

    Add beginning tile editor

--- js/tilemapeditor.js
+++ js/tilemapeditor.js
@@ -1,62 +1,88 @@
-function charToObj(char) {
-  let tile = {},
-      tile_id = "",
-      tile_data = {};
-  if (char === " ") {
-    tile_id = "empty";
-  } else if (char === "g") {
-    tile_id = "grass";
-  } else if (char === "G") {
-    tile_id = "wild_grass";
-  } else if (char === "w") {
-    tile_id = "wall";
-  } else if (char === "p") {
-    tile_id = "portal";
-    tile_data.w_id = prompt("Portal destination world?");
-    tile_data.spawn_id = prompt("Portal spawn ID?");
-  } else if (char === "s") {
-    tile_id = "sign";
-    tile_data.text = prompt("Sign text?");
-    tile_data.ground_tile = charToObj(prompt("Sign ground tile?"));
+"use strict";
+
+var map;
+window.onload = function() {
+  document.getElementById("map").addEventListener("click", handleClick, false);
+};
+
+class Tile {
+  constructor(tileId, color, toJSON=this.defaultJSON) {
+    this.tileId = tileId;
+    this.color = color;
+    this.toJSON = toJSON;
   }
-  tile.tile_id = tile_id;
-  if (Object.keys(tile_data).length !== 0) {
-    tile.tile_data = tile_data;
+
+  defaultJSON() {
+    return {"tile_id": this.tileId};
   }
-  return tile;
+}
+
+class Map {
+  constructor(width, height) {
+    this.tiles = [];
+    this.mapElem = document.getElementById("map");
+    document.getElementById("mapdiv").style.display = "block";
+    this.mapElem.innerHTML = "";
+    for (let i = 0; i < height; i++) {
+      this.addRow();
+      for (let j = 0; j < width; j++) {
+        this.addTile(i);
+      }
+    }
+  }
+
+  addRow() {
+    this.tiles.push([]);
+    let row = document.createElement("TR");
+    this.mapElem.appendChild(row);
+  }
+
+  getRow(rowNum) {
+    return this.mapElem.children[rowNum];
+  }
+
+  addTile(rowNum) {
+    this.tiles[rowNum].push(new Tile("grass", "#00ff00"));
+    let row = this.getRow(rowNum),
+        elem = document.createElement("TD");
+    elem.style.backgroundColor = "#00ff00";
+    row.appendChild(elem);
+  }
+
+  getTile(rowNum, colNum) {
+    return this.mapElem.children[rowNum].children[colNum];
+  }
+
+  setTile(rowNum, colNum, tile) {
+    let elem = this.getTile(rowNum, colNum);
+    elem.style.backgroundColor = tile.color;
+    this.tiles[rowNum][colNum] = tile;
+  }
+}
+
+function handleClick(e) {
+  let cellClicked = e.target,
+      rowClicked = cellClicked.parentNode,
+      table = rowClicked.parentNode,
+      colNum = Array.from(rowClicked.children).indexOf(cellClicked),
+      rowNum = Array.from(table.children).indexOf(rowClicked),
+      tileId = document.getElementById("tile_id").value,
+      tileColor = document.getElementById("tile_color").value,
+      tile = new Tile(tileId, tileColor);
+  map.setTile(rowNum, colNum, tile);
 }
 
 function makeMap() {
-  let map = document.getElementById("map").value,
-      lines = map.split(/\r?\n/g),
-      obj = {version: "0.0.0"};
-
-  let tiles = [];
-  for (const line of lines) {
-    let rowTiles = []
-    for (const char of line) {
-      rowTiles.push(charToObj(char));
-    }
-    tiles.push(rowTiles);
-  }
-  obj.tiles = tiles;
-
-  let spawnPoints = {},
-      inputSpawnPoints = true;
-  while(inputSpawnPoints) {
-    let spawnPointId = prompt("Spawn point ID?"),
-        spawnPoint = {
-          "block_x": parseInt(prompt("Spawn point block_x?")),
-          "block_y": parseInt(prompt("Spawn point block_y?"))
-        };
-    spawnPoints[spawnPointId] = spawnPoint;
-    inputSpawnPoints = prompt("More spawn points?") === "y";
-  }
-  obj.spawn_points = spawnPoints;
-
-  document.getElementById("outputPara").display = "block";
-  outputTextarea = document.getElementById("output");
-  outputTextarea.style.display = "block";
-  outputTextarea.value = JSON.stringify(obj);
+  const width = parseInt(document.getElementById("width").value),
+        height = parseInt(document.getElementById("height").value);
+  map = new Map(width, height);
 }
 
+function makeOutput() {
+  const outputDiv = document.getElementById("outputdiv"),
+        outputBox = document.getElementById("output");
+  outputDiv.style.display = "block";
+  let jsonTiles = map.tiles.map(row => row.map(tile => tile.toJSON()));
+
+  outputBox.value = JSON.stringify(jsonTiles);
+}
